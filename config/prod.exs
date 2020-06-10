7057fb5ec6c43ebd322a8593c8a4787a9e5b4bbd
Clinton <clinton@axisavant.com>
2020-06-10 00:24:28 -0600

    Try running prod on port 4000 because Gigalixir.

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :web, Web.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "example.com", port: 4000],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # ## SSL Support
